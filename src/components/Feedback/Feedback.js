5ad4258deabb83dbbbe23da7230dd3953d970e7c
Igor Napolov <isnapolov@gmail.com>
2022-02-09 17:28:59 +0200

    step 1
    step 1

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import styles from './Feedback.module.css'
+
+class Feedback extends React.Component{
+
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    };
+
+    goodFeedback = () => {        
+        this.setState((prevState) => {
+            return {
+                good: prevState.good + 1,
+            };
+        })
+    };
+    neutralFeedback = () => {
+        this.setState((prevState) => {
+            return {
+                neutral: prevState.neutral + 1,
+            };            
+        })
+    };
+    badFeedback = () => {
+        this.setState((prevState) => {
+            return {
+                bad: prevState.bad+1,    
+            };
+        })
+    };
+
+    render() {
+        return (
+            <div className={styles.container}>
+      <h2>Please leave feedback</h2>
+      <button type="button" onClick={this.goodFeedback} className={styles.button}>Good</button>
+      <button type="button" onClick={this.neutralFeedback} className={styles.button}>Neutral</button>
+      <button type="button" onClick={this.badFeedback} className={styles.button}>Bad</button>
+      <h2>Statistics</h2>
+                <p>Good: <span>{this.state.good}</span></p> 
+                <p>Neutral: <span>{this.state.neutral}</span></p>
+                <p>Bad: <span>{this.state.bad}</span></p>
+    </div>
+
+        )
+    };
+};
+
+export default Feedback;
